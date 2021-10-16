34456d6a2fbad420849bb6fc9c02e5f2b02da5cd
今天不用写代码 <67060479+sbcxk@users.noreply.github.com>
2021-10-16 01:04:46 -0500

    Add files via upload

---
+++ 盲盒交友/3.1前端修复版/static/api.js
@@ -0,0 +1,58 @@
+
+export default (url, method, params) => {
+	uni.showLoading({
+		title: "加载中"
+	});
+	
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url:  "http://localhost:9002"+ url,
+			method: method,
+			header: {
+				"content-type": "application/x-www-form-urlencoded"
+			},
+			data: params,
+			withCredentials:true,
+			success(res) {
+				if (res.data.code == 500) {
+					uni.showToast({
+						title: res.data.msg,
+						icon: "none"
+					})
+				} else if (res.data.code == 407) {
+					uni.showToast({
+						title: res.data.msg,
+						icon: "none"
+					})
+					uni.redirectTo({
+						url:"/pages/index/index"
+					})
+					uni.removeStorageSync("user");
+					
+
+				} else if (res.data.code == 404 || res.data.code == 403) {
+					uni.showToast({
+						title: res.data.msg,
+						icon: "none"
+					})
+					
+				} else {
+					resolve(res);
+				}
+
+			},
+			fail(err) {
+				uni.hideLoading();
+				uni.showToast({
+					title: "操作失败,错误："+err,
+					icon: "error"
+				})
+			},
+			complete(res) {
+				resolve(res);
+				uni.stopPullDownRefresh()
+				uni.hideLoading();
+			}
+		});
+	});
+};
